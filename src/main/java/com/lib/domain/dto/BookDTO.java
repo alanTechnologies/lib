8ed266071a1aff82fb64d4c6449b9fb012f6c963
alexandru.chesnoiu <alexandru.chesnoiu@eos-it.ro>
2021-10-05 19:04:34 +0300

    LIB-6: added bookDTO java class

---
+++ src/main/java/com/lib/domain/dto/BookDTO.java
@@ -0,0 +1,18 @@
+package com.lib.domain.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class BookDTO {
+
+    private Long id;
+    private String name;
+    private Integer age;
+    private String gender;
+    private String university;
+    private Integer numberOfLateReturnings;
+}
