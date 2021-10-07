9ed2df13a37244c06a73332ea8f1573a91218e2d
alexandru.chesnoiu <alexandru.chesnoiu@eos-it.ro>
2021-10-07 17:52:17 +0300

    LIB-24: added ClientController

---
+++ src/main/java/com/lib/system/controller/ClientController.java
@@ -0,0 +1,30 @@
+package com.lib.system.controller;
+
+import com.lib.system.entity.Client;
+import com.lib.system.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
+@CrossOrigin(origins = "*")
+public class ClientController {
+
+    private final ClientService clientService;
+
+    @Autowired
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    @GetMapping("/clients")
+    public List<Client> findAllClients() {
+        return clientService.getAllClients();
+    }
+}
+
