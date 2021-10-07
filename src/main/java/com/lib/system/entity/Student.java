5d8d29b152169d47ced17cf7dc37ee8f388b6f11
Ionut <ciobanuionut99@gmail.com>
2021-10-07 17:26:01 +0300

    LIB-21 Created Entity for Student Tabel In DB

---
+++ src/main/java/com/lib/system/entity/Student.java
@@ -0,0 +1,38 @@
+package com.lib.system.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table (name="STUDENT",schema="lib")
+@Builder
+public class Student {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private Integer age;
+
+    @Column
+    private String genre;
+
+    @Column
+    private String university;
+
+    @Column
+    private Integer numberOfLateReturnings;
+
+
+}
