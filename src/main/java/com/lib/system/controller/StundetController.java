2d339ff50143d1a24596a0100963b5b7919c8f96
alexandru.chesnoiu <alexandru.chesnoiu@eos-it.ro>
2021-10-07 19:40:20 +0300

    LIB-28: added controller for stundet

---
+++ src/main/java/com/lib/system/controller/StundetController.java
@@ -0,0 +1,30 @@
+package com.lib.system.controller;
+
+import com.lib.system.entity.Student;
+import com.lib.system.services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
+@CrossOrigin(origins = "*")
+public class StundetController {
+
+    private final StudentService studentService;
+
+    @Autowired
+    public StundetController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping("/students")
+    public List<Student> getAllStudente() {
+        return studentService.getAllStudents();
+    }
+}
+
