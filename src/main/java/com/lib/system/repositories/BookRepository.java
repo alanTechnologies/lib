02a85419db2fc7e4140e9a27a9f13b83d0902c7a
Ionut <ciobanuionut99@gmail.com>
2021-10-09 13:27:35 +0300

    LIB-33 Created added ignoreCase to byAuthor

--- src/main/java/com/lib/system/repositories/BookRepository.java
+++ src/main/java/com/lib/system/repositories/BookRepository.java
@@ -9,5 +9,5 @@ import java.util.List;
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
-    List<Book> getAllByAuthorContaining(String author);
+    List<Book> getAllByAuthorContainingIgnoreCase(String author);
 }
