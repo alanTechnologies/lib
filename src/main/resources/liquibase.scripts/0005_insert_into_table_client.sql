339553afc7cc3e2bef52ecad95c26a3301bb2a76
steffanciprian <steffanciprian@gmail.com>
2021-10-11 12:57:32 +0300

    deleted StudentController so Ionut can create it

--- src/main/resources/liquibase.scripts/0005_insert_into_table_client.sql
+++ src/main/resources/liquibase.scripts/0005_insert_into_table_client.sql
@@ -8,4 +8,4 @@ VALUES('Arthur Kenneth Vanderwall', 23, 'Male', 1234567891234),
         ('Patricia Parker', 20, 'Female', 9876543219876),
         ('Kye Snyder', 25, 'Male', 2222333344441),
         ('Roza Burch', 21, 'Female', 1222233334444),
-        ('Gloria Mercer', 24, 'Female', 3222233334444);
+        ('Gloria Mercer', 24, 'Female', 1941023440018);
